810c8fa1bc30448f621a5caec902275857e50149
Anoopt <anuanoopthoppilanu@gmail.com>
2026-08-10 23:14:03 +0530

    feat: add menu system for startup management and expand RAM optimization with telemetry/bloatware removal

--- clear_ram_loop.bat
+++ clear_ram_loop.bat
@@ -20,6 +20,23 @@ schtasks /Change /TN "Microsoft\Windows\WindowsUpdate\sih" /Disable >nul 2>&1
 schtasks /Change /TN "Microsoft\Windows\WindowsUpdate\sihboot" /Disable >nul 2>&1
 
 echo [✓] Windows Update Disabled.
+
+:: === One-time: Disable Telemetry & Unnecessary Services ===
+echo [*] Disabling Telemetry and SysMain (Superfetch)...
+sc stop DiagTrack >nul 2>&1
+sc config DiagTrack start= disabled >nul 2>&1
+sc stop dmwappushservice >nul 2>&1
+sc config dmwappushservice start= disabled >nul 2>&1
+sc stop WerSvc >nul 2>&1
+sc config WerSvc start= disabled >nul 2>&1
+sc stop MapsBroker >nul 2>&1
+sc config MapsBroker start= disabled >nul 2>&1
+sc stop SysMain >nul 2>&1
+sc config SysMain start= disabled >nul 2>&1
+
+reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\DataCollection" /v AllowTelemetry /t REG_DWORD /d 0 /f >nul
+
+echo [✓] Telemetry Disabled.
 echo.
 
 :loop
@@ -42,7 +59,7 @@ ipconfig /flushdns >nul
 
 :: === Clear RAM ===
 echo [*] Attempting RAM Trim...
-powershell -command "Get-Process | Where-Object { $_.Id -ne $PID } | ForEach-Object { try { $_.MinWorkingSet = $_.MinWorkingSet } catch {} }"
+powershell -command "Get-Process | Where-Object { $_.Id -ne $PID -and $_.ProcessName -notmatch 'InputApp' } | ForEach-Object { try { $_.MinWorkingSet = $_.MinWorkingSet } catch {} }"
 
 :: === Show RAM usage after ===
 echo [*] RAM Usage After:
@@ -54,10 +71,46 @@ taskkill /f /im OneDrive.exe >nul 2>&1
 taskkill /f /im Teams.exe >nul 2>&1
 taskkill /f /im AdobeIPCBroker.exe >nul 2>&1
 taskkill /f /im AdobeCollabSync.exe >nul 2>&1
-taskkill /f /im ArmouryCrate.UserSessionHelper.exe
-taskkill /f /im GameSDK.exe
-taskkill /f /im NahimicService.exe
-taskkill /f /im AuraService.exe
+
+:: ASUS/ROG Bloat
+taskkill /f /im ArmouryCrate.UserSessionHelper.exe >nul 2>&1
+taskkill /f /im GameSDK.exe >nul 2>&1
+taskkill /f /im NahimicService.exe >nul 2>&1
+taskkill /f /im AuraService.exe >nul 2>&1
+
+:: HP/OMEN Bloat
+taskkill /f /im SysInfoCap.exe >nul 2>&1
+taskkill /f /im TouchpointAnalyticsClientService.exe >nul 2>&1
+taskkill /f /im OmenInstallMonitor.exe >nul 2>&1
+taskkill /f /im OmenCap.exe >nul 2>&1
+taskkill /f /im HPCommRecovery.exe >nul 2>&1
+taskkill /f /im DiagsCap.exe >nul 2>&1
+taskkill /f /im AppHelperCap.exe >nul 2>&1
+taskkill /f /im NetworkCap.exe >nul 2>&1
+taskkill /f /im osprivacy.exe >nul 2>&1
+taskkill /f /im HP.OMEN.Overlay.OverlayHelper.exe >nul 2>&1
+taskkill /f /im HPInsights.exe >nul 2>&1
+
+:: PC Manager Clutter
+taskkill /f /im MSPCManager.exe >nul 2>&1
+taskkill /f /im MSPCManagerCore.exe >nul 2>&1
+taskkill /f /im MSPCManagerService.exe >nul 2>&1
+
+:: Third-Party / Background Misc
+taskkill /f /im warp-svc.exe >nul 2>&1
+taskkill /f /im kdeconnectd.exe >nul 2>&1
+taskkill /f /im kdeconnect-indicator.exe >nul 2>&1
+taskkill /f /im CrossDeviceResume.exe >nul 2>&1
+taskkill /f /im OfficeClickToRun.exe >nul 2>&1
+taskkill /f /im CompatTelRunner.exe >nul 2>&1
+taskkill /f /im bcastdvr.exe >nul 2>&1
+taskkill /f /im SECOMNService.exe >nul 2>&1
+taskkill /f /im smartscreen.exe >nul 2>&1
+
+:: Services Stop
+net stop Spooler /y >nul 2>&1
+net stop WSearch /y >nul 2>&1
+net stop HPInsightsAnalytics /y >nul 2>&1
 
 :: === Run RamMap to clear standby list ===
 echo [*] Running RamMap64 to clear standby memory...
@@ -66,8 +119,13 @@ echo [*] Running RamMap64 to clear standby memory...
 reg add "HKCU\Software\Sysinternals\RamMap" /v EulaAccepted /t REG_DWORD /d 1 /f >nul
 
 :: Run RamMap silently with the -Et -Es -Em -Ew flag to clear everything
-"D:\WindBatchCustom\RAMMap64.exe" -Et -Es -Em -Ew
+"%~dp0RAMMap64.exe" -Et -Es -Em -Ew
 
-echo [✓] Cleanup Done. Sleeping for 10 seconds...
+echo [✓] Cleanup Done. 
+
+:: === Ensure InputApp (Emoji/Dictation) isn't killed ===
+powershell -command "Start-Process 'WindowsInternal.ComposableShell.Experiences.TextInput.InputApp.exe' -ErrorAction SilentlyContinue"
+
+echo [*] Sleeping for 10 seconds...
 timeout /t 10 >nul
 goto loop
